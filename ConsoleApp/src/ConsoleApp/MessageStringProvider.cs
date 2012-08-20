3fd369914e386b86dc15d341e8ba6618e675ddd7
stepanov sergey <sergey.stepanov@adx-team.com>
2012-08-20 13:18:40 +0300

    message string provider

---
+++ ConsoleApp/src/ConsoleApp/MessageStringProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class MessageStringProvider
+    {
+        public virtual string Message
+        {
+            get
+            {
+                return "MessageStringProvider";
+            }
+        }
+    }
+}
